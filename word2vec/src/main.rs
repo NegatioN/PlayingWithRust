f57a17d2d08f2af8c578fa515408333f41f89f43
NegatioN <joakimrishaug@gmail.com>
2020-02-08 09:18:54 +0100

    map data

--- word2vec/src/main.rs
+++ word2vec/src/main.rs
@@ -10,7 +10,7 @@ fn make_lookup<'a>(arr: &[&'a str]) -> HashMap<&'a str, i32> {
     let mut counter = 0;
     for word in arr.iter() {
         if !words.contains_key(word) {
-            words.insert(word.clone(), counter);
+            words.insert(word, counter);
             counter += 1;
         }
     }
@@ -18,30 +18,39 @@ fn make_lookup<'a>(arr: &[&'a str]) -> HashMap<&'a str, i32> {
     words
 }
 
-fn reverse_lookup(lookup: HashMap<&str, i32>) -> HashMap<i32, &str> {
+fn reverse_lookup<'a>(lookup: &HashMap<&'a str, i32>) -> HashMap<i32, &'a str> {
     let mut words: HashMap<i32, &str> = HashMap::new();
-    for (k,v) in lookup.iter() {
+    for (k, v) in lookup.iter() {
         words.insert(*v, k);
     }
     words
 }
 
 fn main() {
-    let ex_sentence = ["i", "walked", "on", "the", "magical", "white", "shore", "in", "my", "white", "shoes", "and", "white", "sky"];
+    let ex_sentence = [
+        "i", "walked", "on", "the", "magical", "white", "shore", "in", "my", "white", "shoes",
+        "and", "white", "sky",
+    ];
     const NUM_WORDS: usize = 1000;
     const VEC_SIZE: usize = 100;
     const NUM_ELEMENTS: usize = NUM_WORDS * VEC_SIZE;
 
-
-    let mut test = Array1::<f64>::zeros(NUM_ELEMENTS);
+    let mut test = Array1::zeros(NUM_ELEMENTS);
     println!("{:?}", test);
     test += 1.0;
     println!("{:?}", test);
     println!("{:?}", test.slice(s![..32]));
 
-
     let words: HashMap<&str, i32> = make_lookup(&ex_sentence);
-    let rev = reverse_lookup(words);
+    let rev: HashMap<i32, &str> = reverse_lookup(&words);
+
+    let mapped_sentence = ex_sentence
+        .iter()
+        .map(|x| words.get(x).unwrap())
+        .collect::<Vec<&i32>>();
+
+    println!("{:?}", mapped_sentence);
+
     //words.insert()
 
     // let vecs: CsMat<f32> = CsMat::zero((num_words, vec_size));
