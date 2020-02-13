60c1101e6eea2ed5039a9359885fc978b24382c9
NegatioN <joakimrishaug@gmail.com>
2020-02-13 16:23:22 +0100

    Stuff

--- word2vec/src/main.rs
+++ word2vec/src/main.rs
@@ -1,6 +1,8 @@
+extern crate circular_queue;
 extern crate ndarray;
 extern crate ndarray_rand;
-use ndarray::{s, Array, Array1, ArrayBase, ArrayViewMut};
+use circular_queue::CircularQueue;
+use ndarray::{s, Array, Array1};
 use ndarray_rand::rand::distributions::Uniform;
 use ndarray_rand::RandomExt;
 
@@ -28,44 +30,89 @@ fn reverse_lookup<'a>(lookup: &HashMap<&'a str, i32>) -> HashMap<i32, &'a str> {
     words
 }
 
+fn sigmoid(x: &f32) -> f32 {
+    1. / (1. + (-x).exp())
+}
+
+fn deriv_logsigmoid(x: &f32) -> f32 {
+    1. / (1. + x.exp())
+}
+
 struct Network {
     output_embeddings: Array1<f32>,
     hidden_embeddings: Array1<f32>,
     vec_size: i32,
     num_words: i32,
+    lr: f32,
+    hidden_grads: Vec<Array1<f32>>,
+    output_grads: Vec<Array1<f32>>,
+    hidden_grads_offsets: Vec<i32>,
+    output_grads_offsets: Vec<i32>,
 }
 
 impl Network {
-    fn new(num_words: i32, vec_size: i32) -> Self {
+    fn new(num_words: i32, vec_size: i32, lr: f32) -> Self {
         let vec_max_val = 0.5 / num_words as f32;
         let udist = Uniform::from(-vec_max_val..vec_max_val);
         let num_elements = num_words * vec_size;
         let mut output_embeddings = Array1::<f32>::zeros(num_elements as usize);
         let mut hidden_embeddings = Array::random(num_elements as usize, udist);
+        let mut hidden_grads = Vec::new();
+        let mut output_grads = Vec::new();
+        let mut hidden_grads_offsets = Vec::new();
+        let mut output_grads_offsets = Vec::new();
         Network {
             output_embeddings,
             hidden_embeddings,
             vec_size,
             num_words,
+            lr,
+            hidden_grads,
+            output_grads,
+            hidden_grads_offsets,
+            output_grads_offsets,
         }
     }
 
-    fn forward(&mut self, ind: &i32, tar_ind: &i32) -> f32 {
+    fn forward(&mut self, ind: &i32, tar_ind: &i32, label: f32) -> f32 {
+        //TODO confirm values goes to correct embedding spaces.
+        let vec_offset = ind * self.vec_size;
+        let tar_offset = tar_ind * self.vec_size;
         let vec = self
-            .output_embeddings
-            .slice(s![ind * self.vec_size..(ind + 1) * self.vec_size]);
-        let tar_vec = self
-            .output_embeddings
-            .slice(s![tar_ind * self.vec_size..(tar_ind + 1) * self.vec_size]);
-        tar_vec.dot(&vec)
+            .hidden_embeddings
+            .slice(s![vec_offset..vec_offset + self.vec_size]);
+        // multiply by label to get inverted vecs if negative sample
+        let tar_vec = label
+            * &self
+                .output_embeddings
+                .slice(s![tar_offset..tar_offset + self.vec_size]);
+
+        let dot = tar_vec.dot(&vec);
+        //TODO implement robust version of logsigmoid
+        //TODO WRITE TESTS, especially for functions like sigmoid
+        let sig = sigmoid(&dot);
+        let loss = -sig.log2();
+
+        //gradient of dot product = lr * (label - score)
+        //positives want to be labeled 1, and negatives will be inverted, and move towards -1
+        let grad = self.lr * (1. - deriv_logsigmoid(&loss));
+        self.hidden_grads.push(grad * &tar_vec);
+        self.output_grads.push(grad * &vec);
+        self.hidden_grads_offsets.push(tar_offset);
+        self.output_grads_offsets.push(vec_offset);
+        loss
     }
 
-    // the opposite vector * error
-    // the error equals logsigmoid(res) distance from 0
-    //the paper says pos +
-    //fn backward(&mut self, posneg_gradients){
-
-    //}
+    fn backward(&mut self) {
+        for _ in 0..self.hidden_grads.len() {
+            let offset = self.hidden_grads_offsets.pop();
+            let vec = self.hidden_grads.pop();
+            self.output_embeddings[offset..offset + self.vec_size] += &vec;
+            self.output_embeddings
+                .slice_mut(s![offset..offset + self.vec_size]) += &vec;
+        }
+        //TODO in backward just apply updates from all stored offsets and grads?
+    }
 }
 
 fn main() {
@@ -77,8 +124,10 @@ fn main() {
     const VEC_SIZE: i32 = 100;
     const WINDOW_SIZE: i32 = 1;
     const NUM_NEG: i32 = 3;
+    const LR: f32 = 0.001;
+    const EPOCHS: usize = 5;
 
-    let mut net = Network::new(NUM_WORDS, VEC_SIZE);
+    let mut net = Network::new(NUM_WORDS, VEC_SIZE, LR);
     let words: HashMap<&str, i32> = make_lookup(&ex_sentence);
     let rev: HashMap<i32, &str> = reverse_lookup(&words);
 
@@ -88,46 +137,42 @@ fn main() {
         .collect::<Vec<&i32>>();
 
     println!("{:?}", mapped_sentence);
-
-    //let select_output = |tar| output_embeddings.slice(s![tar * VEC_SIZE..(tar + 1) * VEC_SIZE]);
-    //let select_hidden = |tar| hidden_embeddings.slice_mut(s![tar * VEC_SIZE..(tar + 1) * VEC_SIZE]);
-
     let max_s = mapped_sentence.len() as i32;
-    for i in 0..max_s {
-        let target = mapped_sentence[i as usize];
-        //let target_vec = select_vec(target, hidden_embeddings, &VEC_SIZE);
-        let mut local_gradient = Array1::<f32>::zeros(VEC_SIZE as usize); //neu1e
-        for j in i - WINDOW_SIZE..i + WINDOW_SIZE + 1 {
-            // Only take valid samples, and not target ind
-            if j >= 0 && j < max_s && j != i {
-                let pos = mapped_sentence[j as usize];
-                for z in 0..NUM_NEG + 1 {
-                    let vec_ind: &i32;
-                    if z == 0 {
-                        vec_ind = pos;
-                    } else {
-                        // negatives can be anything except pos and target
-                        // should be drawn according to distribution and params
-                        vec_ind = &8; // TODO DRAW NEGATIVE RANDOMLY
+
+    let mut losses = CircularQueue::with_capacity(500);
+    for e in 0..EPOCHS {
+        for i in 0..max_s {
+            let target = mapped_sentence[i as usize];
+            for j in i - WINDOW_SIZE..i + WINDOW_SIZE + 1 {
+                // Only take valid samples, and not target ind
+                if j >= 0 && j < max_s && j != i {
+                    let pos = mapped_sentence[j as usize];
+                    for z in 0..NUM_NEG + 1 {
+                        let vec_ind: &i32;
+                        let label: f32;
+                        if z == 0 {
+                            vec_ind = pos;
+                            label = 1.;
+                        } else {
+                            // negatives can be anything except pos and target
+                            // should be drawn according to distribution and params
+                            vec_ind = &8; // TODO DRAW NEGATIVE RANDOMLY
+                            label = -1.;
+                        }
+                        //TODO keep losses to display progress.
+                        let loss = net.forward(vec_ind, target, label);
+                        losses.push(loss);
+
+                        //SUPER-TODO, we need to keep the lists of gradients outside the model
+                        // and then join them when the threads get joined?
                     }
-                    let res = net.forward(vec_ind, target);
-
-                    //WHAT DO I NEED TO STORE TO KEEP FOR BACKWARDS??
-                    // result of dot-product
-                    // to have access to the vector
-                    // the accumulated gradient over pos, neg * targets?
-                    // the target index to apply gradient on
-
-                    //let vec = select_output(vec_ind);
-                    //let res = target_vec.dot(&vec);
-                    let g = res; //TODO more complex gradient
-                                 //TODO COMBINE LOCAL GRADIENT WITH VECTOR
-                                 //local_gradient += &(g * &vec);
-                    println!("rvec {} * {}: {:?}", i, j, res);
                 }
             }
+            println!(
+                "Loss: {}",
+                losses.iter().fold(0., |sum, i| sum + i) / losses.len() as f32
+            );
         }
-        println!("Grad: {:?}", local_gradient);
     }
     //words.insert()
 
